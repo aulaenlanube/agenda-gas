e4ace8c55aace829937e5b2ec00a4aad4e400c53
aulaenlanube <torregrosaedu@gmail.com>
2022-04-07 13:17:22 +0200

    video8 curso gas-avanzado

---
+++ funciones.js
@@ -0,0 +1,9 @@
+function doGet()
+{
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Script');
+}
+
+function obtenerDatosHTML(nombre)
+{
+    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+}
