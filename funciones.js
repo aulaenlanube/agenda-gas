e2ede06225d901b7dcca043861c594824062963f
aulaenlanube <torregrosaedu@gmail.com>
2022-04-08 10:18:28 +0200

    video9 curso gas-avanzado

--- funciones.js
+++ funciones.js
@@ -7,3 +7,10 @@ function obtenerDatosHTML(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos()
+{
+    let hoja = SpreadsheetApp.openById('1Q2dz67BOzarGu4YMYnxXH2Lv1P7XngkYewfPUgc1Dqs').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
