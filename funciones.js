4cb834fcbdf24c6584e9f5f96ff0e42448c52634
aulaenlanube <torregrosaedu@gmail.com>
2022-04-22 12:29:49 +0200

    video18 curso-gas-avanzado

--- funciones.js
+++ funciones.js
@@ -24,3 +24,8 @@ function insertarContacto(nombre, apellidos, correo, telf)
 {
     HOJA.appendRow([nombre,apellidos,correo,telf]);
 }
+
+function borrarContacto(numFila)
+{
+    HOJA.deleteRow(numFila);
+}
