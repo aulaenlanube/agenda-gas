c240d1b4bebe180436dede07dfc305f54bf29ddf
aulaenlanube <torregrosaedu@gmail.com>
2022-05-06 17:35:26 +0200

    video28 curso-gas-avanzado

--- funciones.js
+++ funciones.js
@@ -24,25 +24,27 @@ function obtenerDatos()
 
 function insertarContacto(contacto, imagen)
 {
-    if(imagen)
-    {
-        let blob = Utilities.newBlob(imagen.datos, imagen.tipo, imagen.nombre);
-        let archivo = CARPETA.createFile(blob);
-        contacto.imagen = CABECERA_URL_IMAGEN_DRIVE+archivo.getId();
-    }
-
+    if(imagen) contacto.imagen = guardarImagen(imagen);
     HOJA.appendRow([contacto.nombre, contacto.apellidos, contacto.correo, contacto.telf, contacto.imagen]);
+}
+
+function modificarContacto(contacto, imagen)
+{
+    if(imagen) contacto.imagen = guardarImagen(imagen);
+    let celdas = HOJA.getRange('A'+contacto.fila+':E'+contacto.fila);
+    celdas.setValues([[contacto.nombre, contacto.apellidos, contacto.correo, contacto.telf, contacto.imagen]]);
+}
+
+function guardarImagen(imagen)
+{
+    let blob = Utilities.newBlob(imagen.datos, imagen.tipo, imagen.nombre);
+    let archivo = CARPETA.createFile(blob);
+    return CABECERA_URL_IMAGEN_DRIVE+archivo.getId();
 }
 
 function borrarContacto(numFila)
 {
     HOJA.deleteRow(numFila);
-}
-
-function modificarContacto(numFila,datos)
-{
-    let celdas = HOJA.getRange('A'+numFila+':D'+numFila);
-    celdas.setValues([[datos.nombre, datos.apellidos, datos.correo, datos.telf]]);
 }
 
 function importarContactos()
